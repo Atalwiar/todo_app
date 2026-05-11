2ed00ae3478c473f23d886bf64263110c3266cfe
Atalwiar077 <Atalwiar145@gmail.com>
2026-05-11 17:58:38 +0430

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from "path";
+import mongodb from "mongodb";
+import { MongoClient, ObjectId } from 'mongodb';
+const app = express();
+const mypublic = path.resolve('public');
+app.use(express.json());
+app.use(express.static(mypublic));
+app.use(express.urlencoded({extended: true}));
+app.set("view engine","ejs");
+
+app.get('/',(req,res)=>{
+  res.render("lists");
+})
+
+app.get('/add',(req,res)=>{
+  res.render("add");
+})
+
+app.get('/update',(req,res)=>{
+  res.render("update");
+})
+
+app.post('/add_task',(req,res)=>{
+  res.redirect("/");
+
+})
+
+app.listen(8080,()=>{
+  console.log("server is running in 8080");
+  
+})
